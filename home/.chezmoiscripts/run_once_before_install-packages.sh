22db589053687ab0f7d84511079431626f53d23e
gingerich <marlin.gingerich@gmail.com>
2021-12-26 14:57:49 -0500

    add yaml config

--- home/.chezmoiscripts/run_once_before_install-packages.sh
+++ home/.chezmoiscripts/run_once_before_install-packages.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install zgenom
-if [ ! -d "${HOME}/.zgenom" ]
-then
+if [ ! -d "${HOME}/.zgenom" ]; then
     git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
 else
     echo "Skipping zgenom installation since ${HOME}/.zgenom already exists."
